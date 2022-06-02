94091967596c58bb574eb97dbf297ee149f92e66
Bhawana Bhardwaj <b.bhardwaj@bluewin.ch>
2022-06-02 11:39:38 +0200

    s working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show new create edit update destroy ]
+  before_action :set_book, only: %i[ show create edit update destroy ]
 
   def index
     @books = Book.all

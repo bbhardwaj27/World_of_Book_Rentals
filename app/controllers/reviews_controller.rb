96814b06ccd943c84745a39e3ae9fee99d6fff16
Bhawana Bhardwaj <b.bhardwaj@bluewin.ch>
2022-06-05 11:51:50 +0200

    s working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,13 +11,17 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @book = Book.find(params[:book_id])
-    @review.book = @book
+    @review.book_id = params[:book_id]
+    @review.comment = params[:comment]
+    logger.info "Book Id : " + params[:book_id] + " Comment : " + params[:review][:comment]
     if @review.save
+      logger.info "Save worked Yippe !"
       redirect_to book_path(@book)
     else
-      logger.info "We are in ReviewsController"
-      render 'books/show'
-      render 'review/show'
+      logger.info "Save failed !"
+      # render 'books/show'
+      # render 'review/show'
+      redirect_to book_path(@book)
     end
   end
 
@@ -30,6 +34,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:comment, :rating)
+    # params.require(:comment).permit(:rating)
   end
 end
